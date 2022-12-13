4405d5b9c273b6d5ba73743c85dff212f9aad93a
Carol254 <wanguic49@gmail.com>
2022-12-13 23:16:17 +0300

    added some new pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './dashboard/home/home.component';
+import { ShopComponent } from './dashboard/shop/shop.component';
+import { AboutUsComponent } from './dashboard/about-us/about-us.component';
+import { InsightsComponent } from './dashboard/insights/insights.component';
+import { ContactUsComponent } from './dashboard/contact-us/contact-us.component';
+import { HeaderComponent } from './dashboard/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    HomeComponent,
+    ShopComponent,
+    AboutUsComponent,
+    InsightsComponent,
+    ContactUsComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
