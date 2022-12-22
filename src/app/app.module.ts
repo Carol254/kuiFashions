a7cdc08a76965f3871689d77a51094465dfe135c
Caroline <wanguic49@gmail.com>
2022-12-22 17:08:54 +0300

    created routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { APP_BASE_HREF } from '@angular/common';
+import { RouterModule,Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -9,6 +11,26 @@ import { AboutUsComponent } from './dashboard/about-us/about-us.component';
 import { InsightsComponent } from './dashboard/insights/insights.component';
 import { ContactUsComponent } from './dashboard/contact-us/contact-us.component';
 import { HeaderComponent } from './dashboard/header/header.component';
+import { SignUpComponent } from './dashboard/sign-up/sign-up.component';
+import { SignInComponent } from './dashboard/sign-in/sign-in.component';
+
+const appRoutes:Routes =[
+  {path:'',redirectTo:'dashboard',pathMatch:'full'},
+  {
+    path:'dashboard',
+    children:[
+      {path:'',redirectTo:'home',pathMatch:'full'},
+      {path:'home',component:HomeComponent},
+      {path:'shop',component:ShopComponent},
+      {path:'about-us',component:AboutUsComponent},
+      {path:'insights',component:InsightsComponent},
+      {path:'contact-us',component:ContactUsComponent}
+    ], 
+  },
+  {path:'dashboard/home/sign-up',component:SignUpComponent},
+  {path:'dashboard/home/sign-in',component:SignInComponent}
+
+];
 
 @NgModule({
   declarations: [
@@ -19,12 +41,17 @@ import { HeaderComponent } from './dashboard/header/header.component';
     AboutUsComponent,
     InsightsComponent,
     ContactUsComponent,
-    HeaderComponent
+    HeaderComponent,
+    SignUpComponent,
+    SignInComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
   ],
-  providers: [],
+  providers: [
+    {provide: APP_BASE_HREF, useValue: '/'} 
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/dashboard/header/header.component.ts
+++ src/app/dashboard/header/header.component.ts
@@ -1,4 +1,6 @@
+import { ThisReceiver } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
+  }
+  onSignIn(){
+    this.router.navigate(['dashboard/home/sign-in']);
+  }
+  onSignUp(){
+    this.router.navigate(['dashboard/home/sign-up']);
   }
 
 }
