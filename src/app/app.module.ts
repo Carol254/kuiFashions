884f8f995166004b5fffee83178f44e83fe9efcb
Carol254 <wanguic49@gmail.com>
2022-12-25 21:09:47 +0300

    changes to sign-up form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { APP_BASE_HREF } from '@angular/common';
 import { RouterModule,Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -47,6 +48,7 @@ const appRoutes:Routes =[
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     RouterModule.forRoot(appRoutes),
   ],
   providers: [
--- src/app/dashboard/sign-up/sign-up.component.ts
+++ src/app/dashboard/sign-up/sign-up.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.css']
 })
+
 export class SignUpComponent implements OnInit {
+ 
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() { 
+  
   }
 
+  ngOnInit(): void {
+   
+  }
+
+  onSubmit(f:NgForm){
+    console.log(f);
+  }
 }
