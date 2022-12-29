86622746930238b49abbdf848128e481384215e5
Carol254 <wanguic49@gmail.com>
2022-12-29 21:31:16 +0300

    changes to sign-in

--- src/app/dashboard/sign-in/sign-in.component.ts
+++ src/app/dashboard/sign-in/sign-in.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-in',
@@ -14,12 +14,14 @@ export class SignInComponent implements OnInit {
   constructor() { }
 
    signinForm = new FormGroup({
-    'username': new FormControl(null),
-    'email': new FormControl(null),
+    'username': new FormControl(null,Validators.required),
+    'email': new FormControl(null,[Validators.required,Validators.email]),
     'gender':new FormControl('female')
   });
   ngOnInit(): void {
  
   }
- 
+ onSubmit(){
+  console.log(this.signinForm);
+ }
 }
