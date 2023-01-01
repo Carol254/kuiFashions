6150252938bda770f97956f3943ac5cb5b316467
Carol254 <wanguic49@gmail.com>
2023-01-01 22:41:39 +0300

    changes made to shop-page

--- src/app/dashboard/shop/shop.component.ts
+++ src/app/dashboard/shop/shop.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuData } from 'src/app/models/menus.model';
 
 @Component({
   selector: 'app-shop',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shop.component.css']
 })
 export class ShopComponent implements OnInit {
+  menus:MenuData[] =[
+    new MenuData('Ragged Jeans','Awesome ripped jeans size-10','/assets/ragged-jeans.jpg'),
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
 }
