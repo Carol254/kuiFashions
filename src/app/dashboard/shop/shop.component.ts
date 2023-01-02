e725df421d13c930f3a996bd435ba841b926b338
Carol254 <wanguic49@gmail.com>
2023-01-02 18:28:11 +0300

    added products to shop-page

--- src/app/dashboard/shop/shop.component.ts
+++ src/app/dashboard/shop/shop.component.ts
@@ -9,6 +9,17 @@ import { MenuData } from 'src/app/models/menus.model';
 export class ShopComponent implements OnInit {
   menus:MenuData[] =[
     new MenuData('Ragged Jeans','Awesome ripped jeans size-10','/assets/ragged-jeans.jpg'),
+    new MenuData('Black sweat top','Sweat top size-10','/assets/black sweat-top.jpg'),
+    new MenuData('Black & White dress','Awesome dress size-8','/assets/black&white dress.jpg'),
+    new MenuData('Denim Top','Awesome denim top size-10','/assets/denim top.jpg'),
+    new MenuData('Grey Suit','grey suit size-10','/assets/grey suit.jpg'),
+    new MenuData('Orange Top','Awesome Orange top size-9','/assets/orange top.jpg'),
+    new MenuData('Polka Top','Awesome Polka top size-12','/assets/polka-dot top.jpg'),
+    new MenuData('Pink Blazer','Awesome Pink Blazer size-9','/assets/pink blazer.jpg'),
+    new MenuData('Pink Top','Awesome Pink top size-9','/assets/pink-top.jpg'),
+    new MenuData('Orange Top','Awesome Orange top size-9','/assets/orange top.jpg'),
+    new MenuData('Stripped Top','Awesome Stripped top size-9','/assets/stripped-top.jpg'),
+    new MenuData('Beige Top','Awesome Orange top size-9','/assets/model_3.jpg'),
   ];
 
   constructor() { }
