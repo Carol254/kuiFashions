50b037e47ac07fd1c1181693cb7c460ce6822fd3
Caroline <wanguic49@gmail.com>
2023-01-18 15:43:06 +0300

    slight changes on shop ts-file

--- src/app/dashboard/shop/shop.component.ts
+++ src/app/dashboard/shop/shop.component.ts
@@ -12,36 +12,9 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./shop.component.css']
 })
 export class ShopComponent implements OnInit  {
-  productArr: {
-    id:string;
-    name:string;
-    description:string;
-    img:string;
-    amount:string;
-  }[] =[];
-
 
   products:any= [];
-    
   
-  shoppingInfo:any;
-  
-
-  menus:MenuData[] =[
-    new MenuData('Ragged Jeans','Awesome ripped jeans size-10','/assets/ragged-jeans.jpg'),
-    new MenuData('Black sweat top','Sweat top size-10','/assets/black sweat-top.jpg'),
-    new MenuData('Black & White dress','Awesome dress size-8','/assets/black&white dress.jpg'),
-    new MenuData('Denim Top','Awesome denim top size-10','/assets/denim top.jpg'),
-    new MenuData('Grey Suit','grey suit size-10','/assets/grey suit.jpg'),
-    new MenuData('Orange Top','Awesome Orange top size-9','/assets/orange top.jpg'),
-    new MenuData('Polka Top','Awesome Polka top size-12','/assets/polka-dot top.jpg'),
-    new MenuData('Pink Blazer','Awesome Pink Blazer size-9','/assets/pink blazer.jpg'),
-    new MenuData('Pink Top','Awesome Pink top size-9','/assets/pink-top.jpg'),
-    new MenuData('Orange Top','Awesome Orange top size-9','/assets/orange top.jpg'),
-    new MenuData('Stripped Top','Awesome Stripped top size-9','/assets/stripped-top.jpg'),
-    new MenuData('Beige Top','Awesome Beige top size-9','/assets/model_3.jpg'),
-  ];
-
   constructor(private router:Router, private httpClient:HttpClient,private cartService:CartService) { }
 
   ngOnInit(){
