44f97c94c68122873773958fbfc8e5a4645fa767
Caroline <wanguic49@gmail.com>
2023-01-31 22:41:13 +0300

    added authentication service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -9,4 +9,37 @@ import { Injectable } from '@angular/core';
 export class AuthService {
 
   constructor(private fireauth:AngularFireAuth,private router:Router) { }
+  //login Method
+  logIn(email:string,password:string){
+    this.fireauth.signInWithEmailAndPassword(email,password).then(()=>{
+      localStorage.setItem('token','true');
+      this.router.navigate(['dashboard/shop/cart/checkout']);
+    },err=>{
+      alert(err.message);
+      this.router.navigate(['dashboard/home/sign-in']);
+    })
+  }
+//register method
+  signUp(email:string,password:string){
+    this.fireauth.createUserWithEmailAndPassword(email,password).then(()=>{
+      alert('Registration successfull');
+    this.router.navigate(['dashboard/home/sign-in']);
+
+    },
+    err=>{
+      alert(err.message);
+      this.router.navigate(['dashboard/home/sign-up']);
+    })
+  }
+
+  //logOut
+
+  logOut(){
+    this.fireauth.signOut().then(()=>{
+      localStorage.removeItem('token');
+      this.router.navigate(['dashboard/home/sign-in']);
+    },err=>{
+      alert(err.message);
+    })
+  }
 }
