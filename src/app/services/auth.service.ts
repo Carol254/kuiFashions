3cc54362fbadb5251bff6383033d354dc17468c9
Caroline <wanguic49@gmail.com>
2023-02-04 22:45:29 +0300

    changes in auth service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -15,11 +15,7 @@ export class AuthService {
     this.fireauth.signInWithEmailAndPassword(email,password).then(res =>{
       localStorage.setItem('token','true');
 
-      if (res.user?.emailVerified == true){
-        this.router.navigate(['dashboard/shop/cart/checkout']);
-      } else {
-        this.router.navigate (['dashboard/verify-email']);
-      }
+
 
     },err=>{
       alert(err.message);
@@ -32,7 +28,6 @@ export class AuthService {
     this.fireauth.createUserWithEmailAndPassword(email,passsword).then(res =>{
       alert('Registration successfull');
       this.router.navigate(['dashboard/home/sign-in']);
-      this.sendEmailForVerification(res.user);
     },err =>{
       alert(err.message);
       this.router.navigate(['dashboard/home/sign-up']);
@@ -59,13 +54,6 @@ export class AuthService {
     })
   }
 
-  sendEmailForVerification(user:any){
-    user.sendEmailVerification().then((res:any)=>{
-      this.router.navigate(['dashboard/verify-email']);
-    },(err:any)=>{
-      alert('Something went wrong,not able to register email');
-    }
-    )
-  }
+
 
 }
