fa1e4a6d085f313eadd42ff40f39b46ccae8e74a
Caroline <wanguic49@gmail.com>
2023-02-07 16:54:09 +0300

    changes made to checkout page

--- src/app/dashboard/checkout/checkout.component.ts
+++ src/app/dashboard/checkout/checkout.component.ts
@@ -7,6 +7,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CheckoutComponent implements OnInit {
 
+  counties =[
+            "Mombasa",
+            "Kwale",
+            "Kilifi",
+            "Tana River",
+            "Lamu",
+            "Taita Taveta",
+           " Garissa",
+           " Wajir",
+          "  Mandera",
+           " Marsabit",
+           " Isiolo",
+          "  Meru",
+           " Tharaka-Nithi",
+           " Embu",
+           " Kitui",
+           " Machakos",
+           " Makueni",
+           " Nyandarua",
+            "Nyeri",
+            "Kirinyaga",
+            "Murang’a",
+            "Kiambu",
+            "Turkana",
+           " West Pokot",
+           " Samburu",
+           " Trans-Nzoia",
+           " Uasin Gishu",
+            "Elgeyo-Marakwet",
+           " Nandi",
+            "Baringo",
+          "Laikipia",
+            "Nakuru",
+            "Narok",
+            "Kajiado",
+            "Kericho",
+            "Bomet",
+            "Kakamega",
+            "Vihiga",
+            "Bungoma",
+            "Busia",
+            "Siaya",
+            "Kisumu",
+           " Homa Bay",
+           " Migori",
+            "Kisii",
+            "Nyamira",
+            "Nairobi"
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
